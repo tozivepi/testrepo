a161d6a0b8415cdf404baae1283e86bb6700c129
tozivepi <89837373+tozivepi@users.noreply.github.com>
2021-08-31 11:23:52 +0200

    Add files via upload

---
+++ TextFile1.py
@@ -0,0 +1,29 @@
+from random import shuffle
+
+mylist = [' ','O',' ']
+
+def shuffle_list(mylist):
+    # Take in list, and returned shuffle versioned
+    shuffle(mylist)
+    
+    return mylist
+
+def player_guess():
+    
+    guess = ''
+    
+    while guess not in ['0','1','2']:
+        
+        # Recall input() returns a string
+        guess = input("Pick a number: 0, 1, or 2:  ")
+    
+    return int(guess) 
+
+
+def check_guess(mylist,guess):
+    if mylist[guess] == 'O':
+        print('Correct Guess!')
+    else:
+        print('Wrong! Better luck next time')
+        print(mylist)
+
